e595f2cd13e0d2b51f60fb71bebf79def62fefb4
mali <mahlhel@gmail.com>
2020-11-16 05:34:12 +0300

    push nati arabic font pro module

---
+++ nati_arabic_font_pro/__manifest__.py
@@ -0,0 +1,28 @@
+ # -*- coding: utf-8 -*-
+{
+    'name': "Nati arabic font Pro",
+
+    'summary': """
+        change defult font to nice arabic font""",
+
+    'description': """
+        Change the defult arabic font of the all interfaces with a beautiful one preferred by the Arabic user
+ ,
+    """,
+    'author': "Mali ,MuhlhelITS",
+    'website': "http://natigroup.biz",
+    'price': 100.00,
+    'currency': 'EUR',
+     'license': 'OPL-1',
+    'category': 'Localization',
+    'version': '14.0.0.0',
+    'depends': ['web'],
+    'qweb': [],
+    'data': ['views/linkestatic.xml',   
+    ],
+    'images': ['static/description/banner.png'],
+    'auto_install': True,
+    'installable': True,
+
+
+}
